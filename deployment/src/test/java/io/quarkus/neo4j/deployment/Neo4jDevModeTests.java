32c9c8d261733c71e1cc7908d2bcddb61a67fb86
Michael Simons <michael@simons.ac>
2022-08-03 11:42:40 +0200

    refactor: Add tests to make sure plugins install correctly when using dev services.
    Closes #81.

--- deployment/src/test/java/io/quarkus/neo4j/deployment/Neo4jDevModeTests.java
+++ deployment/src/test/java/io/quarkus/neo4j/deployment/Neo4jDevModeTests.java
@@ -30,6 +30,7 @@ public class Neo4jDevModeTests {
         static QuarkusUnitTest test = new QuarkusUnitTest()
                 .withEmptyApplication()
                 .withConfigurationResource("application.properties")
+                .overrideConfigKey("quarkus.neo4j.devservices.additional-env.NEO4JLABS_PLUGINS", "[\"apoc\"]")
                 .setLogRecordPredicate(record -> true)
                 .assertLogRecords(records -> assertThat(records).extracting(LogRecord::getMessage)
                         .contains("Dev Services started a Neo4j container reachable at %s"));
@@ -41,6 +42,10 @@ public class Neo4jDevModeTests {
         public void shouldBeAbleToConnect() {
 
             assertThatNoException().isThrownBy(() -> driver.verifyConnectivity());
+            try (var session = driver.session()) {
+                var apoc = session.run("RETURN apoc.version() AS output").single().get(0).asString();
+                assertThat(apoc).isNotNull().startsWith("4.");
+            }
         }
     }
 

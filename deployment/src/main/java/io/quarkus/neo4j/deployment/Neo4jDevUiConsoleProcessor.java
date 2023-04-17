fd4b7ac55e60351c2b0b3455261e280569da9c04
Michael Simons <michael@simons.ac>
2023-04-17 14:08:25 +0200

    refactor: Migrate to new dev-ui.
    Closes #156.

---
+++ deployment/src/main/java/io/quarkus/neo4j/deployment/Neo4jDevUiConsoleProcessor.java
@@ -0,0 +1,35 @@
+package io.quarkus.neo4j.deployment;
+
+import java.util.List;
+
+import io.quarkus.deployment.IsDevelopment;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.DevServicesResultBuildItem;
+import io.quarkus.devui.spi.page.CardPageBuildItem;
+import io.quarkus.devui.spi.page.Page;
+
+class Neo4jDevUiConsoleProcessor {
+
+    @BuildStep(onlyIf = IsDevelopment.class)
+    CardPageBuildItem create(
+            List<DevServicesResultBuildItem> runningDevServices,
+            Neo4jBuildTimeConfig neo4jBuildTimeConfig) {
+
+        var cardPageBuildItem = new CardPageBuildItem();
+        if (Neo4jDevServicesProcessor.enabled(neo4jBuildTimeConfig.devservices)) {
+
+            // Find the appropriate config
+            for (DevServicesResultBuildItem runningDevService : runningDevServices) {
+                if (runningDevService.getConfig().containsKey(Neo4jDevServicesProcessor.NEO4J_BROWSER_URL)) {
+                    cardPageBuildItem.addPage(Page.externalPageBuilder("Neo4J Browser")
+                            .icon("font-awesome-solid:diagram-project")
+                            .url(runningDevService.getConfig().get(Neo4jDevServicesProcessor.NEO4J_BROWSER_URL))
+                            .doNotEmbed());
+                    break;
+                }
+            }
+        }
+
+        return cardPageBuildItem;
+    }
+}

a195d57b9e81e4b858960994c671d2b6014f6890
Michael J. Simons <michael@simons.ac>
2024-08-26 14:36:44 +0200

    Migrate to ConfigMapping using interface because, well idk.

--- deployment/src/main/java/io/quarkus/neo4j/deployment/DevServicesBuildTimeConfig.java
+++ deployment/src/main/java/io/quarkus/neo4j/deployment/DevServicesBuildTimeConfig.java
@@ -5,45 +5,47 @@ import java.util.Optional;
 import java.util.OptionalInt;
 
 import io.quarkus.runtime.annotations.ConfigGroup;
-import io.quarkus.runtime.annotations.ConfigItem;
+import io.smallrye.config.WithDefault;
 
 @ConfigGroup
-public class DevServicesBuildTimeConfig {
+public interface DevServicesBuildTimeConfig {
 
     /**
      * If DevServices has been explicitly enabled or disabled. DevServices is generally enabled
      * by default, unless there is an existing configuration present.
      * When DevServices is enabled Quarkus will attempt to automatically configure and start
      * a database when running in Dev or Test mode.
+     *
+     * @return whether dev services are enabled or not
      */
-    @ConfigItem
-    public Optional<Boolean> enabled = Optional.empty();
+    Optional<Boolean> enabled();
 
     /**
-     * The container image name to use, for container based DevServices providers.
+     * {@return the container image name to use, for container based DevServices providers}
      */
-    @ConfigItem(defaultValue = "neo4j:5")
-    public String imageName;
+    @WithDefault("neo4j:5")
+    String imageName();
 
     /**
-     * Additional environment entries that can be added to the container before its start.
+     * {@return additional environment entries that can be added to the container before its start}
      */
-    @ConfigItem
-    public Map<String, String> additionalEnv;
+    Map<String, String> additionalEnv();
 
     /**
      * This value can be used to specify the port to which the bolt-port of the container is exposed. It must be a free
      * port, otherwise startup will fail. A random, free port will be used by default. Either way, a messsage will be
      * logged on which port the Neo4j container is reachable over bolt.
+     *
+     * @return a specific port to bind the containers bolt-port to
      */
-    @ConfigItem
-    public OptionalInt boltPort = OptionalInt.empty();
+    OptionalInt boltPort();
 
     /**
      * This value can be used to specify the port to which the http-port of the container is exposed. It must be a free
      * port, otherwise startup will fail. A random, free port will be used by default. Either way, a messsage will be
      * logged on which port the Neo4j Browser is available.
+     *
+     * @return a specific port to bind the containers http-port to
      */
-    @ConfigItem
-    public OptionalInt httpPort = OptionalInt.empty();
+    OptionalInt httpPort();
 }
